f2fb390f0fdeb943cf97442f29edcefa11f7739f
Mateusz Nowak <mateusz.nowak@justjoin.it>
2023-03-07 15:56:55 +0100

    add src

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,44 @@
-import * as core from "@actions/core";
-import { wait } from "./wait";
-import { execSync } from "child_process";
-import { generateTypeScriptCode } from "./generators/ts/ts-generator";
+import * as core from '@actions/core'
+import {wait} from './wait'
+import {execSync} from 'child_process'
+import {generateTypeScriptCode} from './generators/ts/ts-generator'
+
+interface GitHubActionInput {
+  readonly in: InProps
+  readonly out: OutProps
+}
+
+interface InProps {
+  readonly files: string[] // files - can be pattern
+}
+
+interface OutProps {
+  readonly language: 'csharp' | 'typescript'
+  readonly mode: 'just-files' | 'package'
+  readonly package: string // package name / namespace
+  // one file or many???
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput("milliseconds");
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const ms: string = core.getInput('milliseconds')
+    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
+    core.debug(new Date().toTimeString())
+    await wait(parseInt(ms, 10))
+    core.debug(new Date().toTimeString())
 
     //core.setOutput('time', new Date().toTimeString())
-    const output = execSync("ls", { encoding: "utf-8" }); // the default is 'buffer'
+    const output = execSync('ls', {encoding: 'utf-8'}) // the default is 'buffer'
 
-    core.setOutput("Output was:\n", output);
+    core.setOutput('Output was:\n', output)
 
     // check langugage - if csharp and option package is enabled
     // validate if
-    await generateTypeScriptCode();
+    await generateTypeScriptCode()
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message);
+    if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
-run();
+run()
