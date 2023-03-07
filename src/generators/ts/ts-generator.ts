e90d5f1d4d38773545153414cc6788618c6a7fa1
Mateusz Nowak <mateusz.nowak@justjoin.it>
2023-03-07 16:25:12 +0100

    generate files

--- src/generators/ts/ts-generator.ts
+++ src/generators/ts/ts-generator.ts
@@ -1,6 +1,10 @@
-import {TypeScriptGenerator} from '@asyncapi/modelina'
+import { TypeScriptFileGenerator, TypeScriptGenerator } from "@asyncapi/modelina";
+
+const codeGenerator = new TypeScriptGenerator();
+const fileGenerator = new TypeScriptFileGenerator();
 
 export async function generateTypeScriptCode(): Promise<void> {
+
   const jsonSchemaDraft7 = {
     $schema: 'http://json-schema.org/draft-07/schema#',
     type: 'object',
@@ -12,10 +16,9 @@ export async function generateTypeScriptCode(): Promise<void> {
       }
     }
   }
-
-  const generator = new TypeScriptGenerator()
-  const models = await generator.generate(jsonSchemaDraft7)
+  const models = await codeGenerator.generate(jsonSchemaDraft7)
   for (const model of models) {
     console.log(model.result)
   }
+  await fileGenerator.generateCompleteModels(models, {});
 }
