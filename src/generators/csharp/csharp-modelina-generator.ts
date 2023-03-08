9877cf3ef544e5024b1f1c4bdee17c6208c99fd4
Mateusz Nowak <mateusz.nowak@justjoin.it>
2023-03-08 14:58:24 +0100

    playground with c#

--- src/generators/csharp/csharp-modelina-generator.ts
+++ src/generators/csharp/csharp-modelina-generator.ts
@@ -2,6 +2,7 @@ import {
   CSharpFileGenerator,
   ConstrainedDictionaryModel
 } from '@asyncapi/modelina'
+import fs from "fs";
 
 const generator = new CSharpFileGenerator({
   collectionType: 'List',
@@ -38,5 +39,22 @@ ${renderer.indent(renderer.renderBlock(content, 2))}
 })
 
 export async function generateCSharpCode(): Promise<void> {
+  const outDir = "./__tests__/out/message-schema/generated/csharp/modelina/Test.cs";
+  const inDir = "./__tests__/assets";
+
+  const allJsonFiles = fs.readdirSync(inDir).filter((file) => file.includes(".json"));
+  const allJsonSchemas = allJsonFiles.map(file => [file, JSON.parse(fs.readFileSync(`${inDir}/${file}`, 'utf8'))]);
   
+  await Promise.all(
+    allJsonSchemas.map(async ([fileName, jsonSchema]) => {
+      const models = await generator.generateToFiles(
+        jsonSchema,
+        outDir,
+        {}
+      )
+      for (const model of models) {
+        console.log(model.result)
+      }
+    })
+  );
 }
