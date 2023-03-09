6e3435fef29cbf7c614d3455a2287f4d819f1576
Mateusz Nowak <mateusz.nowak@justjoin.it>
2023-03-09 12:53:44 +0100

    ts as type

--- src/generators/typescript/typescript-modelina-generator.ts
+++ src/generators/typescript/typescript-modelina-generator.ts
@@ -1,14 +1,36 @@
 import {
-  TypeScriptGenerator
+  TypeScriptGenerator, TypeScriptOptions
 } from "@asyncapi/modelina";
 import { CodeGeneratorConfig, CodeGeneratorResult } from "../shared/code-generator";
+import { TypeScriptObjectRenderer } from "@asyncapi/modelina/lib/types/generators/typescript/TypeScriptObjectRenderer";
+import { InterfacePresetType } from "@asyncapi/modelina/lib/types/generators/typescript/TypeScriptPreset";
 
+
+// HACK: interface renderer to type renderer
+export const TS_INTERFACE_AS_READONLY_TYPE_PRESET: InterfacePresetType<TypeScriptOptions> =
+  {
+    async self({ renderer, options, model }) {
+      const content = [
+        await renderer.renderProperties(),
+        await renderer.runAdditionalContentPreset()
+      ];
+
+      return `export type ${model.name.replaceAll("Minus", "")} = Readonly<{
+${renderer.indent(renderer.renderBlock(content, 2))}
+}>`;
+    }
+  };
 
 const generator = new TypeScriptGenerator({
   modelType: "interface",
   renderTypes: true,
   enumType: "union",
-  mapType: "indexedObject"
+  mapType: "indexedObject",
+  presets: [
+    {
+      interface: TS_INTERFACE_AS_READONLY_TYPE_PRESET
+    }
+  ]
 });
 
 export async function generateTypeScriptCode(config: CodeGeneratorConfig): Promise<CodeGeneratorResult> {
@@ -16,7 +38,7 @@ export async function generateTypeScriptCode(config: CodeGeneratorConfig): Promi
   const generatedModels = await Promise.all(jsonSchemaFiles
     .map(async (file) => {
       const { name, content } = file;
-      const models = await generator.generateCompleteModels(content, { exportType: "named" });
+      const models = await generator.generate(content);
       return { file, models };
     }));
   return {
--- src/generators/typescript/typescript-quicktypecli-generator.ts
+++ src/generators/typescript/typescript-quicktypecli-generator.ts
@@ -1,7 +1,5 @@
-import * as cp from "child_process";
 import { executeSystemCommand } from "../shared/command-executor";
 
-// todo: three steps (model), then writing to file (one or many)
 export async function generateTypeScriptCode(): Promise<void> {
   const inFile = "./__tests__/assets/*.json";
   const outDir = "./__tests__/out/message-schema/generated/typescript/quicktype";
